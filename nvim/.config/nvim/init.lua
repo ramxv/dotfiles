0699a9ad80e713bf2508842c22ea76b31e2d3057
Ram Singh <ramxv.singh@gmail.com>
2025-08-20 00:56:52 -0500

    inital commit

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,53 @@
+--[[
+=====================================================================
+==================== READ THIS BEFORE CONTINUING ====================
+=====================================================================
+========                                    .-----.          ========
+========         .----------------------.   | === |          ========
+========         |.-""""""""""""""""""-.|   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||   KICKSTART.NVIM   ||   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||                    ||   |-----|          ========
+========         ||:Tutor              ||   |:::::|          ========
+========         |'-..................-'|   |____o|          ========
+========         `"")----------------(""`   ___________      ========
+========        /::::::::::|  |::::::::::\  \ no mouse \     ========
+========       /:::========|  |==hjkl==:::\  \ required \    ========
+========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
+========                                                     ========
+=====================================================================
+=====================================================================
+--]]
+
+--[ For more information about this kickstart.nvim init.lua visit: https://github.com/nvim-lua/kickstart.nvim?tab=readme-ov-file ]
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- [[Setting filetypes]]
+require 'kickstart.config.filetypes'
+
+-- [[ Setting options ]]
+require 'kickstart.config.options'
+
+-- [[ Setting keymaps ]]
+require 'kickstart.config.keymaps'
+
+-- [[ Setting autocmd ]]
+require 'kickstart.config.autocmd'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
